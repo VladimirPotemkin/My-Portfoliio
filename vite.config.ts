b1c190f4da2f7aa42eba1a5e9c24db46c4d5689a
Vladimir Potemkin <vapotemkin@akontech.ru>
2026-05-05 16:26:57 +0300

    vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spa-resume/',
+  base: '/My-Portfoliio/',
   plugins: [vue(), tailwindcss()],
 })
